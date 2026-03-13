99d177f52fc218aa068cbed5e13987f8129c2349
Dilnuwan Weerasuriya <dilnuwanweerasuriya@gmail.com>
2026-03-13 16:54:09 +0530

    Updates

--- src/components/Services/ServicesHero.jsx
+++ src/components/Services/ServicesHero.jsx
@@ -7,8 +7,8 @@ export default function ServicesHero() {
       <Hero
             page="services"
             pageTitle="Services"
-            img="https://images.unsplash.com/photo-1764222233275-87dc016c11dc?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070&q=80"
-            // img="/images/hero/services.png"
+            // img="https://images.unsplash.com/photo-1764222233275-87dc016c11dc?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070&q=80"
+            img="/images/hero/services.webp"
             imgAlt="Real Estate Services"
             badgeText="What We Offer"
             title="Expertise that Shapes"
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -413,7 +413,7 @@ export default function Events() {
       <Hero
         page="events"
         pageTitle="Events"
-        img="https://images.unsplash.com/photo-1591389703635-e15a07b842d7?q=80&w=1633&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070&q=80"
+        img="/images/hero/events.jpeg"
         imgAlt="Events"
         badgeText="Our Legacy"
         title="Previous Events"
